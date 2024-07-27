ea7c6982f1441d2e10cf4ce07b7238f8c377fa14
Satyam Singh Rawat <142081382+satyamsiuu@users.noreply.github.com>
2024-07-27 13:14:46 +0530

    Update and rename lol.c to p1.cpp

---
+++ TCS-307/26-07-2024/p1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter values for a and b"<<endl;
+    cin>>a>>b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is = "<<a+b;
+    return 0;
+}
