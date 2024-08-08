928f72922af19cc3f64c83ff4bc2d39eb541918e
Satyam Singh Rawat <142081382+satyamsiuu@users.noreply.github.com>
2024-08-08 19:12:26 +0530

    Create p2b.cpp

---
+++ PCS-307/08-08-2024/p2b.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double sum=0,fact=1;
+    int n,a;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        fact*=i;
+        sum+=a/fact;
+    }
+    cout<<"Sum of the series = "<<sum<<endl;
+    return 0;
+}
