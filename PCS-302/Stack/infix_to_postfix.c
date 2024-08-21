c4c108601e5a66f4897499ec68b83fcef0e4b5fa
Satyam Singh Rawat <142081382+satyamsiuu@users.noreply.github.com>
2024-08-21 11:04:56 +0530

    Update infix_to_postfix.c

--- PCS-302/Stack/infix_to_postfix.c
+++ PCS-302/Stack/infix_to_postfix.c
@@ -2,80 +2,61 @@
 #include<string.h>
 #define max 100
 void push(char,char[],int*);
-void operator(int*,int*,int*,int*,int*,char[],char[],char[]);
+int precedence(char);
 char pop(char[],int*);
-char seek(char[],int*);
 int main()
 {
 	char infix[max],postfix[max],stack[max],temp,c;
-	int l=0,top=0,h=0,p=4,pp;
-	printf("\nEnter infix notation:-\n");
+	int l=0,top=0,h=0;
+	printf("Enter infix notation:-\n");
 	fgets(infix,max,stdin);
 	l=strlen(infix);
 	infix[l-1]=')';
 	stack[0]='(';
 	for(int i=0;i<l;i++)
 	{
-		if((infix[i]>='a'&&infix[i]<='z')||(infix[i]>='A'&&infix[i]<='Z'))
-			postfix[h++]=infix[i];
+		c=infix[i];
+		if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+			postfix[h++]=c;
 		
-		if(infix[i]=='(')
-			push(infix[i],stack,&top);
+		else if(c=='(')
+			push(c,stack,&top);
 		
-		if(infix[i]==')')
+		else if(c==')')
 		{
-			while(seek(stack,&top)!='(')
+			while(stack[top]!='(')
 			{
 				temp=pop(stack,&top);
 				postfix[h++]=temp;
 			}
 			pop(stack,&top);
-			pp=4;
 		}
-		
-		if(infix[i]=='+'||infix[i]=='-')
+		else 
 		{
-			p=3;
-			operator(&p,&pp,&i,&h,&top,stack,infix,postfix);
-		}
-			
-		if(infix[i]=='/'||infix[i]=='*'||infix[i]=='%')
-		{
-			p=2;
-			operator(&p,&pp,&i,&h,&top,stack,infix,postfix);
-		}
-			
-		
-		if(infix[i]=='^')
-		{
-			p=1;
-			operator(&p,&pp,&i,&h,&top,stack,infix,postfix);
+			while(precedence(stack[top])>precedence(c))
+			{
+				temp=pop(stack,&top);
+				postfix[h++]=temp;
+			}
+			push(c,stack,&top);
 		}
 	}
 	
 	printf("\nThe postfix notation is:-\n");
-	for(int i=0;i<h;i++)
-		printf("%c",postfix[i]);
-	printf("\n\n");
+	postfix[h]='\0';
+	puts(postfix);
 	return 0;
 }
-void operator(int *p,int *pp,int *i,int* h,int *top,char stack[],char infix[],char postfix[])
+int precedence(char x)
 {
-	char temp;
-	if(*pp<*p)
-	{
-		while(seek(stack,top)!='(')
-		{		
-			temp=pop(stack,top);
-			postfix[(*h)++]=temp;
-		}
-		*pp=4;
-	}
-	else
-	{
-		push(infix[*i],stack,top);
-		*pp=*p;
-	}
+	if(x=='^')
+		return 5;
+	else if(x=='%'||x=='*'||x=='/')
+		return 4;
+	else if(x=='+'||x=='-')
+		return 3;
+	else 
+		return 2;
 }
 void push(char x,char stack[],int *top)
 {
@@ -93,11 +74,4 @@ char pop(char stack[],int *top)
 		temp=stack[(*top)--];
 	return temp;
 }
-char seek(char stack[],int *top)
-{
-	if(*top==-1)
-		printf("Underflow during seek\n");
-	return stack[*top];
-}
-		
-		
+						
