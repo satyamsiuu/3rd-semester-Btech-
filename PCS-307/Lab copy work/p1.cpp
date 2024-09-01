ef64f3cd16eeb0e460f9feae8f38ada6173f46f9
Satyam Singh Rawat <142081382+satyamsiuu@users.noreply.github.com>
2024-09-01 22:32:23 +0530

    Update p1.cpp

--- PCS-307/Lab copy work/p1.cpp
+++ PCS-307/Lab copy work/p1.cpp
@@ -9,28 +9,70 @@ then an additional surcharge of 15% is added.
 Implement a C++ program to read the names of users and number of units consumed
 and display the charges with names
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+struct users
+{
+    string name;
+    int units;
+    double cost = 0.0;
+};
+
 int main()
 {
-	int units;
-	double cost;
-	cout<<"Enter Units: ";
-	cin>>units;
-	if(units>300)
-		cost=units*0.9;
-	else if(units>100)
-		cost=units*0.8;
-	else
-		cost=units*0.6;
-		
-	if(cost>300)
-		cost= cost*1.15;
-	else if(cost<50)
-		cost=50;
-	
-	cout<<"Total amount of electricity: Rs "<<cost<<endl;
-	return 0;
+    int n, i;
+
+    cout << "Enter number of users: ";
+    cin >> n;
+
+    users *a = new users[n];
+
+    if (a == nullptr) {
+        cout << "Memory allocation failed!" << endl;
+        return 1;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter name of user: ";
+        cin >> a[i].name;
+        cout << "Enter the number of units consumed: ";
+        cin >> a[i].units;
+
+        if (a[i].units <= 100) 
+		{
+            a[i].cost = a[i].units * 0.60;
+		}
+		else if (a[i].units <= 300) 
+		{
+            a[i].cost = 100 * 0.60 + (a[i].units - 100) * 0.80;
+        } 
+		else 
+		{
+            a[i].cost = 100 * 0.60 + 200 * 0.80 + (a[i].units - 300) * 0.90;
+        }
+
+        if (a[i].cost < 50.0) 
+		{
+            a[i].cost = 50.0;
+        }
+
+        if (a[i].cost > 300.0) 
+		{
+            a[i].cost = a[i].cost * 1.15;
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        cout << "Name: " << a[i].name << endl;
+        cout << "Cost: Rs " << a[i].cost << endl << endl;
+    }
+
+    delete a;
+
+    return 0;
 }
 
 	
