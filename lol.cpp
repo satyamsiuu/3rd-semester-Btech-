6b80fc809eda7512c8da224800ff740dc1071ac6
Satyam Singh Rawat <142081382+satyamsiuu@users.noreply.github.com>
2024-10-03 13:53:45 +0530

    Update lol.cpp

--- lol.cpp
+++ lol.cpp
@@ -1,59 +1,25 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <iostream>
-using namespace std;
-
-void symmetry(int n)
-{
-    for (int i = 1; i <= 2 * n - 1; i++)
+void getNumberPattern(int n) {
+    for(int i=1;i<=n;i++)
     {
-        for (int j = 1; j <= 2 * n - 1; j++)
+        for(int j=1;j<=i;j++)
+            cout<<n-j+1<<;
+        for(int j=1;j<=2*(n-i-1)+1;j++)
+            cout<<n-i+1;
+        for(int j=1;j<=i;j++)
         {
-            for (int k = 1; k <= 2 * n - 1; k++)
-            {
-                if (k == i)
-                {
-                    if (k % 2 == 0 && k != 1)
-                        cout << (2 * n - i - 1) / 2;
-                    else
-                        cout << (2 * n - i) / 2;
-                    break;
-                }
-                if (k == j)
-                {
-                    if (k % 2 == 0 && k != 1)
-                        cout << (2 * n - j - 1) / 2;
-                    else
-                        cout << (2 * n - j) / 2;
-                    break;
-                }
-                if (k == 2 * n - i)
-                {
-                    cout << (i + 1) / 2;
-                    break;
-                }
-                if (k == 2 * n - j)
-                {
-                    cout << (j + 1) / 2;
-                    break;
-                }
-            }
+            if(n-i+j != 1)
+                cout<<n-i+j;
         }
-        cout << endl;
+        cout<<endl;
     }
-}
-
-int main()
-{
-    int n;
-    cin >> n;
-    symmetry(n);
-
-    return 0;
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=n;j>=i;j--)
+            cout<<j;
+        for(int j=1;j<=2*(i-2)+1;j++)
+            cout<<i;
+        for(int j=i;j<=n;j++)
+            cout<<j;
+        cout<<endl;
+    }
 }
