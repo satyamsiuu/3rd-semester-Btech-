c7fae94dff3f57e20196a106d71ca375c51c39ad
Satyam Singh Rawat <142081382+satyamsiuu@users.noreply.github.com>
2024-10-25 11:48:50 +0530

    Update task2.cpp

--- PCS-307/25_10_2024/task2.cpp
+++ PCS-307/25_10_2024/task2.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
 class Item
@@ -7,48 +7,48 @@ class Item
 	public:
 		string title;
 		int year;
-		
-		Item(string t,int y):title(t),year(y){}
-		
-		void display()
+
+		Item(string t, int y) : title(t), year(y) {}
+
+		virtual void display()
 		{
-			cout<<"Title: "<<title<<"\nYear: "<<year<<endl;
+			cout << "Title: " << title << "\nYear: " << year << endl;
 		}
 };
 
-class Book: public Item
+class Book : public Item
 {
 	public:
 		string author;
-		
-		Book(string t,int y,string a):Item(t,y),author(a){}
-		
-		void display()
+
+		Book(string t, int y, string a) : Item(t, y), author(a) {}
+
+		void display() override
 		{
 			Item::display();
-			cout<<"Author: "<<author<<endl;
+			cout << "Author: " << author << endl;
 		}
 };
 
-class ReferenceBook: public Book
+class ReferenceBook : public Book
 {
 	public:
 		string subject;
-		
-		ReferenceBook(string t,int y,string a,string s):Book(t,y,a),subject(s){}
-		
-		void display()
+
+		ReferenceBook(string t, int y, string a, string s) : Book(t, y, a), subject(s) {}
+
+		void display() override
 		{
-			cout<<"Reference Book Details:-\n"<<endl;
+			cout << "Reference Book Details:\n" << endl;
 			Book::display();
-			cout<<"Subject "<<subject<<endl<<endl;
+			cout << "Subject: " << subject << endl << endl;
 		}
 };
 
 int main()
 {
-	ReferenceBook *obj1 = new ReferenceBook("Let US C",1990,"Yashvant Kanethkar","C");
-	(*obj1).display();
+	ReferenceBook *obj1 = new ReferenceBook("Let Us C", 1990, "Yashavant Kanetkar", "C Programming");
+	obj1->display();
 	delete obj1;
 	return 0;
 }
